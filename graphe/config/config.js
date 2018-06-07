f1bf1ecb4f73881570e855bdd9ef1849f8fda28c
Matthew Igho <ighotouch@gmail.com>
2018-06-07 17:22:07 +0100

    fix test

--- graphe/config/config.js
+++ graphe/config/config.js
@@ -32,8 +32,6 @@ config.databaseUrl = `mongodb://${process.env.DB_USERNAME}:${
   process.env.DB_PASSWORD
 }@${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_DATABASE}`;
 
-console.log(config);
-
 config = Object.assign(config, environmentSettings);
 
 // Export final configuration object
